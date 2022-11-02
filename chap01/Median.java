3b3bfeac1df420a993d6765f8644b1832924f4db
jeonga <jjeong0106@gmail.com>
2022-11-02 16:50:10 +0900

    chap01 실습 1C-1 : 3개의 정숫값을 입력하고 중앙값을 구하여 출력

---
+++ chap01/Median.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Median {
+    static int med3(int a, int b, int c) {
+        if (a >= b) {
+            if (b >= c) {
+                return b;
+            }
+            else if (a <= c) {
+                return a;
+            }
+            else {
+                return c;
+            }
+        }
+        else if (a > c) {
+            return a;
+        }
+        else if (b > c) {
+           return c;
+        }
+        else {
+            return b;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdInt = new Scanner(System.in);
+
+        System.out.println("세 정수의 중앙값을 구합니다.");
+
+        System.out.print("a의 값 : ");
+        int a = stdInt.nextInt();
+
+        System.out.print("b의 값 : ");
+        int b = stdInt.nextInt();
+
+        System.out.print("c의 값 : ");
+        int c = stdInt.nextInt();
+
+        System.out.println("중앙값은 " + med3(a, b, c) + "입니다.");
+    }
+}
