3838f1c668e5372a65dff0d6a86f26b65e78e87c
jeonga <jjeong0106@gmail.com>
2022-11-05 18:43:37 +0900

    chap03 실습 3-8 : 신체검사 데이터 배열에서 이진 검색

--- chap03/X.java
+++ chap03/X.java
@@ -1,14 +1,14 @@
-import java.util.Comparator;
-
-class X {
-    // 필드, 메서드 등
-    public static final Comparator<T> COMPARATOR = new Comp();
-
-    private static class Comp implements Comparator<T> {
-        public int compare(T d1, T d2) {
-            // d1이 d2보다 크면 양수 반환
-            // d1이 d2보다 작으면 음수 반환
-            // d1이 d2와 같으면 0 반환
-        }
-    }
-}
+//import java.util.Comparator;
+//
+//class X {
+//    // 필드, 메서드 등
+//    public static final Comparator<T> COMPARATOR = new Comp();
+//
+//    private static class Comp implements Comparator<T> {
+//        public int compare(T d1, T d2) {
+//            // d1이 d2보다 크면 양수 반환
+//            // d1이 d2보다 작으면 음수 반환
+//            // d1이 d2와 같으면 0 반환
+//        }
+//    }
+//}
